b9589ad1b19ddec49d28e5c1098c37faa7a51463
Dov Azogui <dov.azogui@gmail.com>
2024-08-15 11:32:11 +0200

    created the js file

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', clickAlert); // Add the event listener here
+}
+
+function clickAlert() {
+  alert('I was clicked!');
 }
